23334fa983fd58bea4c6286fbdcbf2fd9c0a07e4
Daan Hessen <daanh2002@gmail.com>
2025-10-21 16:08:53 +0200

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./globals.css";
 import HomePage from "./pages/HomePage";
 import SocialMedia from "./components/SocialMedia";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <div className="app-container">
       <HomePage />
       <SocialMedia />
+      <Analytics />
     </div>
   );
 }
