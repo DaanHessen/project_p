bed910396898cf5c23c9c7f2db0847fbae2edce5
Daan Hessen <daanh2002@gmail.com>
2025-10-22 10:49:27 +0200

    enable x

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -38,9 +38,9 @@ const SocialMedia: React.FC = () => {
       ),
     },
     {
-      name: "X (they disabled my account because I am 'under the age of 13'...)",
-      url: "",
-      className: "x disabled",
+      name: "X",
+      url: "https://x.com/Ge_Daan0",
+      className: "x",
       icon: (
         <svg viewBox="0 0 24 24" fill="currentColor">
           <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
